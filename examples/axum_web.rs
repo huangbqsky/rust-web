3524c32e207c74b9c0c8c39c0f47a5566810f6b1
huangbqsky <huangbqsky@gmail.com>
2023-04-20 13:41:58 +0800

    axum web

--- examples/axum_web.rs
+++ examples/axum_web.rs
@@ -1,16 +1,23 @@
 
-use axum::{Router, routing::get, extract::Json, response::IntoResponse};
+use axum::{Router, routing::{get, post}, extract::Json, response::IntoResponse};
 use serde::{Deserialize, Serialize};
+use serde_json::{Value, json};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Hello {
     message: String,
 }
 
-// curl "127.0.0.1:8080/hello" -H "Content-Type: application/json" -d {"Hello":{message:"hello world"}}
-async fn hello(item: Json<Hello>) ->impl IntoResponse {
-    println!("{}", item.message);
-    item 
+// curl "127.0.0.1:8080/hello" -H "Content-Type: application/json" -d {"message":"hello world"}
+async fn echo(item: Json<Hello>) ->impl IntoResponse {
+    println!("{:?}", item);
+    Json(json!({ "data": 42 }))
+}
+
+// `Json` gives a content-type of `application/json` and works with any type
+// that implements `serde::Serialize`
+async fn json() -> Json<Value> {
+    Json(json!({ "data": 42 }))
 }
 
 // curl "127.0.0.1:8080/"
@@ -42,7 +49,8 @@ async fn main() {
     // our router
     let app = Router::new()
       .route("/", get(index))
-      .route("/hello", get(hello))
+      .route("/json", get(json))
+      .route("/echo", post(echo))
       .route("/foo", get(get_foo).post(post_foo))
       .route("/foo/bar", get(foo_bar));
 
