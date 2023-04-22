dc7fda8f04e8b727a9ec3cd06d1aa224476f0500
huangbqsky <huangbqsky@gmail.com>
2023-04-22 18:29:36 +0800

    salvo web request

--- examples/salvo_web.rs
+++ examples/salvo_web.rs
@@ -1,11 +1,112 @@
+use salvo::{macros::Extractible};
 use salvo::prelude::*;
+use serde::{Deserialize, Serialize};
 
 #[handler]
-async fn hello_world() -> &'static str {
+async fn show(req: &mut Request, res: &mut Response) {
+    let content = format!(
+        r#"<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Parse data</title>
+        </head>
+        <body>
+            <h1>Hello, fill your profile</h1>
+            <div id="result"></div>
+            <form id="form" method="post">
+                <label>First Name:</label><input type="text" name="first_name" />
+                <label>Last Name:</label><input type="text" name="last_name" />
+                <legend>What is Your Favorite Pet?</legend>      
+                <input type="checkbox" name="lovers" value="Cats">Cats<br>      
+                <input type="checkbox" name="lovers" value="Dogs">Dogs<br>      
+                <input type="checkbox" name="lovers" value="Birds">Birds<br>    
+                <input type="submit" value="Submit" />
+            </form>
+            <script> 
+            let form = document.getElementById("form");
+            form.addEventListener("submit", async (e) => {{
+                e.preventDefault();
+                let response = await fetch('/{}?username=jobs', {{
+                    method: 'POST',
+                    headers: {{
+                        'Content-Type': 'application/json',
+                    }},
+                    body: JSON.stringify({{
+                        first_name: form.querySelector("input[name='first_name']").value,
+                        last_name: form.querySelector("input[name='last_name']").value,
+                        lovers: Array.from(form.querySelectorAll("input[name='lovers']:checked")).map(el => el.value),
+                    }}),
+                }});
+                let text = await response.text();
+                document.getElementById("result").innerHTML = text;
+            }});
+            </script>
+        </body>
+    </html>
+    "#,
+        req.params().get("id").unwrap()
+    );
+    res.render(Text::Html(content));
+}
+
+#[handler]
+async fn edit<'a>(good_man: GoodMan<'a>, res: &mut Response) {
+    res.render(Json(good_man));
+}
+
+#[derive(Serialize, Deserialize, Extractible, Debug)]
+#[extract(default_source(from = "body", format = "json"))]
+struct GoodMan<'a> {
+    #[extract(source(from = "param"))]
+    id: i64,
+    #[extract(source(from = "query"))]
+    username: &'a str,
+    first_name: String,
+    last_name: String,
+    lovers: Vec<String>,
+    #[extract(source(from = "request"))]
+    nested: Nested<'a>,
+}
+
+#[derive(Serialize, Deserialize, Extractible, Debug)]
+#[extract(default_source(from = "body", format = "json"))]
+struct Nested<'a> {
+    #[extract(source(from = "param"))]
+    id: i64,
+    #[extract(source(from = "query"))]
+    username: &'a str,
+    first_name: String,
+    last_name: String,
+    #[extract(rename = "lovers")]
+    #[serde(default)]
+    pets: Vec<String>,
+}
+
+#[handler]
+async fn hello(req: &mut Request) -> String {
+    let query_id = req.query::<String>("id").unwrap();
+    let param_id = req.params().get("id").cloned().unwrap_or_default();
+    println!("querys id= {:?}, params id = {:?}", query_id, param_id);
+
+    format!("{:?}", req)
+}
+
+#[handler]
+async fn index() -> &'static str {
     "Hello world"
 }
+
 #[tokio::main]
 async fn main() {
-    let router = Router::new().get(hello_world);
-    Server::new(TcpListener::bind("127.0.0.1:7878")).serve(router).await;
+    tracing_subscriber::fmt().init();
+    let addr = "127.0.0.1:7878";
+    println!("listening on {}", addr);
+    // 路由
+    let router = Router::new()
+        .push(Router::new().get(index)) // http://127.0.0.1:7878
+        .push(Router::with_path("hello").get(hello)) // http://127.0.0.1:7878/hello?id=123
+        .push(Router::with_path("users/<id>").get(show).post(edit)); // http://127.0.0.1:7878/users/95
+    Server::new(TcpListener::bind(addr))
+        .serve(router)
+        .await;
 }
